625fd38d0d6d8f97df64bb79c2a775954fbde73a
Guillermo <guille.mca@hotmail.com>
2016-12-10 19:52:18 +0100

    añadido modulo 5

---
+++ Modulo5_LibreriasDeCodigo/ejercicios.js
@@ -0,0 +1,19 @@
+/*autor: guilleCM
+
+Enunciado:
+Desarrolla una librería Croupier que tenga una función para 
+barajar cartas. Para barajar, intercambiamos una carta aleatoria 
+dentro de nuestra baraja por otra. Y esto lo hacemos 100 veces.
+Se recomienda usar la librería Math.random. 
+Por ejemplo, intercambiar la carta en la posición 7 por la 
+carta en la posición 24.
+
+2. Desarrolla nuevas funciones de la librería Croupier: Repartir,
+ que toma la carta en primera posición de la baraja y 
+ la elimina del array Puntuar que, dada una mano de 4 cartas 
+ sume su valor. Pero si todas las cartas son iguales,
+  multiplica el valor por 10.
+
+3. Desarrolla una librería Calculadora que calcula las longitudes
+ de varias figuras geométricas como cuadrados, rectángulos y círculos.
+*/
